3a7ad4c541648dac97cd855d5b2036de317f2b4e
EmilceAide <emilceaide.t@gmail.com>
2023-04-22 12:21:00 -0500

    models

---
+++ src/models/Type.js
@@ -0,0 +1,19 @@
+const { DataTypes } = require('sequelize');
+
+
+module.exports = (database) => {
+ 
+  database.define('Type', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true, 
+      primaryKey: true
+    }, 
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false, 
+      unique: true
+    }
+
+  });
+};
--- src/models/pokemon.js
+++ src/models/pokemon.js
@@ -3,5 +3,43 @@ const { DataTypes } = require('sequelize');
 
 module.exports = (database) => {
  
-  database.define();
+  database.define('Pokemon', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true, 
+      primaryKey: true
+    }, 
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false, 
+      unique: true
+    }, 
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    hp:{
+      type: DataTypes.INTEGER
+    },
+    attack: {
+      type: DataTypes.INTEGER
+    }, 
+    defense: {
+      type: DataTypes.INTEGER
+    }, 
+    speed: {
+      type: DataTypes.INTEGER
+    }, 
+    height: {
+      type: DataTypes.INTEGER
+    },
+    weight: {
+      type: DataTypes.INTEGER
+    }, 
+    type: {
+      type: DataTypes.INTEGER,
+      allowNull: false 
+    }
+
+  });
 };
