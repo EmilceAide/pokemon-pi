982a7034b94ff28a5d24bf9419a578bbbb5f8f95
EmilceAide <emilceaide.t@gmail.com>
2023-04-24 10:07:00 -0500

    port

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const server = require('./src/app');
 const { database } = require('./src/db');
 
+const port = process.env.PORT || 3001;
+
 database.sync({force: true}).then(() => {
 
     console.info('Database connected')
-    server.listen(3001, () => {
-        console.log('Server on port 3001')
+    server.listen(port, () => {
+        console.log(`Server on port ${port}`)
     })
 
 })
