cfc1d9158d45bb7fd302a34abdfed48df81dc3ab
EmilceAide <emilceaide.t@gmail.com>
2023-04-25 10:04:49 -0500

    added: route /types

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,11 @@
 const { Router } = require('express');
-const pokemonsRouter = require('./pokemonsRouter')
+const pokemonsRouter = require('./pokemonsRouter.js');
+const typesRouter = require('./typeRouter.js')
+
 
 const router = Router();
 
-router.use('/pokemons', pokemonsRouter)
+router.use('/pokemons', pokemonsRouter);
+router.use('/types', typesRouter); 
 
 module.exports = router; 
