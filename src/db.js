09e572a4e365c2db6a15f6d435a3b871d8c8d01f
EmilceAide <emilceaide.t@gmail.com>
2023-05-07 23:33:28 -0500

    DB_PORT

--- src/db.js
+++ src/db.js
@@ -2,9 +2,9 @@ require('dotenv').config()
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const {DB_USER, DB_PASSWORD, DB_HOST, DB_NAME} = process.env;
+const {DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT} = process.env;
 
-const database = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
+const database = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
   logging: false
 });
 
